5b5b7ec61785e1f832f82355c0af03d004b4bafd
pendulum <rs.pendulum@gmail.com>
2024-06-15 16:19:56 +0300

    ready upload file

--- src/modules/file-manager/file-manager.interface.ts
+++ src/modules/file-manager/file-manager.interface.ts
@@ -1,5 +1,5 @@
 export abstract class IFileManager {
   abstract upload(file: Express.Multer.File): Promise<void>;
-  abstract read(file: Express.Multer.File): Promise<void>;
-  abstract delete(file: Express.Multer.File): Promise<void>;
+  abstract read(fileName: string, version?: number): Promise<void>;
+  abstract delete(fileName: string, version?: number): Promise<void>;
 }
--- src/modules/file-manager/file-manager.service.ts
+++ src/modules/file-manager/file-manager.service.ts
@@ -1,54 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { IFileManager } from './file-manager.interface';
-import { access, writeFile, readdir } from 'fs-extra';
+import { writeFile, readdir, readFile } from 'fs-extra';
+import * as crypto from 'crypto';
 import { join } from 'path';
 
+type File = Express.Multer.File;
 
 const filePath = `files`
 
 @Injectable()
 export class LocalFileManager extends IFileManager {
-
-  public override async upload(file: Express.Multer.File): Promise<void> {
-    await this.getVersionsFile(file.originalname);
-    
-    const path = join(filePath, file.originalname);
-    const isExists = await this.isFileExist(path);
-    if(!isExists){
-      await writeFile(path, file.buffer);
+  private readonly logger = new Logger(LocalFileManager.name);
+  
+  public override async upload(file: File): Promise<void> {
+    let fileName = '';
+    const files = await this.getVersionFiles(file.originalname);
+    if(files.length > 0){
+      const res = files[files.length - 1];
+      const lastVersion = await readFile(join(filePath,res));
+      const [hashA, hashB] = await Promise.all([
+        this.computeFileHash(file.buffer),
+        this.computeFileHash(lastVersion)
+      ]);
+      if(hashA === hashB){
+        return;
+      }
+      const index = res.indexOf(`_`);
+      const result = Number(res.substring(1, index)) + 1;
+      fileName = `v${result}_${file.originalname}`;
     }
-  }
-
-  public override async read(file: Express.Multer.File): Promise<void> {
-    // Реализация метода чтения файла
-  }
-
-  public override async delete(file: Express.Multer.File): Promise<void> {
-    // Реализация метода удаления файла
-  }
-
-  private async isFileExist(filePath: string): Promise<boolean> {
-    try{
-
-      await access(filePath);
-      return true;
+    else{
+      fileName = `v1_${file.originalname}`;
+    }
+    try {
+      await writeFile(join(filePath, fileName), file.buffer);
     }
     catch(err){
-      return false;
+      this.logger.error(err.message);
     }
   }
-  private async getVersionsFile(fileOriginalName: string){
-    try {
-      const files = await readdir(join(filePath));
-      return files.some(file => file.includes(fileOriginalName));
-    } catch (err) {
-      if (err.code === 'ENOENT') {
-        return false;
-      }
-      throw err;
-    }
-  }
-  private async getLastVersion(){
 
+  public override async read(fileName: string, version?: number): Promise<void> {
+    
+  }
+
+  public override async delete(fileName: string, version?: number): Promise<void> {
+    
+  }
+
+  private async getVersionFiles(fileOriginalName: string): Promise<string[]>{
+    const files = await readdir(join(filePath));
+    return files.filter(file => file.includes(fileOriginalName, 3));
+  }
+
+  private async computeFileHash(buffer: Buffer): Promise<string> {
+    const hashSum = crypto.createHash('sha256')
+    hashSum.update(buffer);
+    const hex = hashSum.digest('hex');
+    return hex;
   }
 }
